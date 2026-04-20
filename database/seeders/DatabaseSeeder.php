90fd57c6f7e607830dc540cc1cd78f613850b2c8
Devies Ade <deviesade@gmail.com>
2026-04-20 16:40:10 +0700

    init

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@edufa.com',
+            'password' => Hash::make('password'),
+            'role' => 'admin',
+            'email_verified_at' => now(),
+        ]);
+    }
+}
---
+++ resources/js/Pages/Guest/Page.jsx
@@ -0,0 +1,12 @@
+// resources/js/Pages/Guest/Page.jsx
+
+export default function Page() {
+    return (
+        <div className="min-h-screen flex items-center justify-center">
+            <h1 className="text-3xl font-bold">
+                Richo tempek
+                
+            </h1>
+        </div>
+    );
+}
