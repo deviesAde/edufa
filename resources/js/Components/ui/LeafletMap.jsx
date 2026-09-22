fb441b2f51b655c60a289d2991270434e389b5dc
Richo Saputra <m.richo.saputra69@gmail.com>
2026-09-22 17:24:01 +0700

    feat: add LeafletMap component for interactive branch mapping

--- resources/js/Components/ui/LeafletMap.jsx
+++ resources/js/Components/ui/LeafletMap.jsx
@@ -48,8 +48,8 @@ export function LeafletMap({ branches, selectedCity, onSelectCity }) {
             >
                 
                 <TileLayer
-                    url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
-                    attribution='&copy; <a href="https://carto.com/attributions">CARTO</a>'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 />
                 
                 {branches.filter(b => (b.latitude || b.lat) && (b.longitude || b.lng)).map((branch, idx) => (
